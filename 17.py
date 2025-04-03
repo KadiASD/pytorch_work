deea166f200a9fc85a2d751f62ea9256df359499
yan20ting <3236695075@qq.com>
2025-04-03 16:15:44 +0800

    1

--- 17.py
+++ 17.py
@@ -17,4 +17,25 @@ def test01():
     print(id(x))
 
 
+def test02():
+    x = torch.tensor([10, 20], requires_grad=True, dtype=torch.float32)
+    print("x-->", x)
+    y = 2 * x ** 2
+    print("y-->", y)
+    y.sum().backward()
+    print("x的梯度值是：", x.grad)
+
+
+def test03():
+    x1 = torch.tensor(10, requires_grad=True, dtype=torch.float64)
+    x2 = torch.tensor(29, requires_grad=True, dtype=torch.float64)
+    y = x1 ** 2 + x2 ** 2 + x1 * x2
+    y = y.sum()
+    y.backward()
+    print("x1.grad-->", x1.grad)
+    print("x2.grad-->", x2.grad)
+
+
 test01()
+test02()
+test03()
